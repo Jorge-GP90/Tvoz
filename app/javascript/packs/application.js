94559d437f43733da31899565a05cc6ae02cd577
George Garcia <jorge.90gap@gmail.com>
2022-02-25 22:48:15 +0900

    testing heroku deploy adding boostrap and  popping/core

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,8 +15,9 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-require("jquery");
-require("bootstrap");
-// require("@rails/ujs").start()
-// require("turbolinks").start()
-// require("@rails/activestorage").start()
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+
+import "bootstrap";
+import "../stylesheets/application";
