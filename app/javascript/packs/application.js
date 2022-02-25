6697628010fbdfc87adf738b17ad24646c293ed0
George Garcia <jorge.90gap@gmail.com>
2022-02-25 22:09:23 +0900

    testing heroku deploy, application.js uncomment jquery and boostrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,8 +15,8 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-// require("jquery");
-// require("bootstrap");
+require("jquery");
+require("bootstrap");
 // require("@rails/ujs").start()
 // require("turbolinks").start()
 // require("@rails/activestorage").start()
