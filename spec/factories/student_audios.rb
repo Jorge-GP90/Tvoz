f6392e22d60bcc3c1744e07a38bbcc7f7d3d7117
George Garcia <jorge.90gap@gmail.com>
2022-02-26 16:44:06 +0900

    testing heroku commented boostrap application.html.erb

--- spec/factories/student_audios.rb
+++ spec/factories/student_audios.rb
@@ -1,7 +1,19 @@
 FactoryBot.define do
   factory :student_audio do
-    audio_student { "MyText" }
+    audio_student { nil }
     user { nil }
     task { nil }
+    association :user
+    association :task
   end
+  factory :student_audio2 do
+    audio_student { nil }
+    association :user
+    association :task
+  end 
+  factory :student_audio3 do
+    audio_student { File.open(Rails.root.join('spec', 'factories', 'audios', 'test_audio.m4a'))}
+    association :user
+    association :task
+  end 
 end
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -10,15 +10,5 @@ FactoryBot.define do
     audio { './storage/test_audio.m4a' }
     image {  '' }
     association :user
-  end  
-  factory :task_image do
-    title { 'Image' }
-    audio { '' }
-    after(:build) do |attached|
-      attached.image.attach(io: File.open(Rails.root.join('spec', 'factories', 'images', 'test2.jpg')), filename: 'test2.jpg', content_type: 'image/jpg')
-    end
-    # image { './storage/test2.jpg' }
-    association :user
-  end
-  
+  end    
 end
